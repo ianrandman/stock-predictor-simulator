5811ab9a4dce36f3f9f44d4d200153eef940e364
Max <maxakipust@gmail.com>
2020-02-08 12:54:06 -0500

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './webapp/App';
+import * as serviceWorker from './webapp/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/webapp/components/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Login extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>LOGIN</h1>
+            </div>
+        )
+    }
+}
