72b61919b02fbffed90492da6462a30088221602
David <dad9489@rit.edu>
2020-02-08 14:55:29 -0500

    remove hardcoded api key

--- AI/get_data.py
+++ AI/get_data.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+with open('./api_key.txt', 'r') as f:
+    api_key = f.readline()
 
-r = requests.get('https://finnhub.io/api/v1/forex/candle?symbol=OANDA:EUR_USD&resolution=D&from=1549584000&to=Unix.Now&token=bovgj2nrh5r90eafkcu0')
+r = requests.get('https://finnhub.io/api/v1/forex/candle?symbol=OANDA:EUR_USD&resolution=D&from=1549584000&to=Unix.Now&token=' + api_key)
 print(json.dumps(r.json(), indent=4))
