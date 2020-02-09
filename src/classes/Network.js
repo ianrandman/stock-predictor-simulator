80830637377ead2c40f9e74db97801ae3aec1ac3
Max Kipust <maxakipust@gmail.com>
2020-02-09 03:05:13 -0500

    yep

--- src/classes/Network.js
+++ src/classes/Network.js
@@ -1,6 +1,6 @@
 export default class Network{
   static  getData = (stock, successCallback, failCallback)=>{
-    window.fetch(`http://f4e46a3c-11e7-47e4-92ab-fd169ac7e057.mock.pstmn.io/data?stock=${stock}`, {mode: 'cors',}).then((resp)=>{
+    window.fetch(`http://f4e46a3c-11e7-47e4-92ab-fd169ac7e057.mock.pstmn.io/data?stock=${stock}`, {mode: 'no-cors',}).then((resp)=>{
       if(resp.ok){
         resp.text().then((json)=>{
           successCallback(json);
@@ -17,6 +17,7 @@ export default class Network{
     window.fetch(`https://api-dot-stock-predictor-simulator.appspot.com/predict?stock=${stock}&date=${date}`, {mode: 'no-cors',}).then((resp)=>{
       if(resp.ok){
         resp.text().then((json)=>{
+          console.log(json);
           successCallback(json);
         });
       }else{
