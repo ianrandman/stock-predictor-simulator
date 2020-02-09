3452b6d5ec7db867bea50d639eade632edde95f6
Max Kipust <maxakipust@gmail.com>
2020-02-09 03:18:27 -0500

    i

--- src/classes/Network.js
+++ src/classes/Network.js
@@ -1,6 +1,6 @@
 export default class Network{
   static  getData = (stock, successCallback, failCallback)=>{
-    window.fetch(`http://f4e46a3c-11e7-47e4-92ab-fd169ac7e057.mock.pstmn.io/data?stock=${stock}`, {mode: 'no-cors',}).then((resp)=>{
+    window.fetch(`https://f4e46a3c-11e7-47e4-92ab-fd169ac7e057.mock.pstmn.io/data?stock=${stock}`, {mode: 'no-cors',}).then((resp)=>{
       if(resp.ok){
         resp.text().then((json)=>{
           successCallback(json);
